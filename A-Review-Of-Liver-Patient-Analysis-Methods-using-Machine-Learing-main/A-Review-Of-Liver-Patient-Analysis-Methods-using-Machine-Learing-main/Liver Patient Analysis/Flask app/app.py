18f5cdcff25f76760dd2a5e8268b293972efd15c
Anburaj2003 <132037969+Anburaj2003@users.noreply.github.com>
2024-07-12 11:20:46 +0530

    Update app.py

--- A-Review-Of-Liver-Patient-Analysis-Methods-using-Machine-Learing-main/A-Review-Of-Liver-Patient-Analysis-Methods-using-Machine-Learing-main/Liver Patient Analysis/Flask app/app.py
+++ A-Review-Of-Liver-Patient-Analysis-Methods-using-Machine-Learing-main/A-Review-Of-Liver-Patient-Analysis-Methods-using-Machine-Learing-main/Liver Patient Analysis/Flask app/app.py
@@ -10,7 +10,7 @@ def index():
 return render_template("index.html")
 @app.route('/data_predict', methods=['POST']) 
 def predict():
-    data = [[float(age), float(gender), flot(tb), float(db), float(ap), float(aa1), float(aa2), 
+    data = [[float(age), float(gender), float(tb), float(db), float(ap), float(aa1), float(aa2), 
          float(tp),
          model =pickle.load(open('liver_analysis.pkl', 'rb'))
          prediction= model.predict(data)[0] 
